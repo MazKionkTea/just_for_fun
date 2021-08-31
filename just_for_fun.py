2b2dde7f314320f787b7ebfaa88a8fec1d0bbb5f
Kionk <47136221+MazKionkTea@users.noreply.github.com>
2021-08-31 19:47:59 +0700

    Add files via upload

---
+++ just_for_fun.py
@@ -0,0 +1,72 @@
+########################################################################
+
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+    def cool_person(self, your_personality):
+        good = "Hard Work", "Humble", "kind", "Nice"
+        if your_personality in good:
+            print(f"{Person(self.name)} {your_personality}, you are Cool...!!!")
+        elif your_personality not in good:
+            print(f"{Person(self.name)} {your_personality}, you are not Cool...!!!")
+        else:
+            print("FUCK YOU...!!!")
+
+    def succsess(self, dedicated, prsistence, patient, passion):
+        dedicated = True
+        prsistence = True
+        patient = True
+        passion = True
+
+def try_again():
+    pass
+
+def try_harder():
+    pass
+
+def never_giveup():
+    pass
+
+def giveup():
+    pass
+
+def you_dead():
+    pass
+
+def you_alive():
+    pass
+
+def sadness():
+    pass
+
+def happyness():
+    pass
+
+def cool_person(your_personality):
+    good = ("Hard Worker", "Humble", "kindess", "Nice", "Grateful")
+    if your_personality in good:
+        print("You are Cool...!!!")
+    elif your_personality not in good:
+        print("You are not Cool...!!!")
+    else:
+        print("FUCK YOU...!!!")
+
+def succsess(dedicated, prsistence, patient, passion):
+    you = ()
+    dedicated = True        # dedicate yourself
+    prsistence = True       # be presistence
+    patient = True          # be patient
+    passion = True          # have passion
+
+    while True:
+        if you is dedicated:
+            print()
+        elif you is prsistence:
+            print()
+        elif you is patient:
+            print()
+        elif you is passion:
+            print()
+        else:
+            never_giveup()
