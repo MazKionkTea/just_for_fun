af2fa894c30e262e5be9bb359df399c8a153f830
Kionk <47136221+MazKionkTea@users.noreply.github.com>
2021-09-11 21:41:37 +0700

    Update just_for_fun.py

--- just_for_fun.py
+++ just_for_fun.py
@@ -1,29 +1,36 @@
 ########################################################################
 
-class Person():
-    def __init__(self, name):
-        self.name = name
-
-    def cool_person(self, your_personality):
-        good = "Hard Work", "Humble", "kind", "Nice"
-        if your_personality in good:
-            print(f"{Person(self.name)} {your_personality}, you are Cool...!!!")
-        elif your_personality not in good:
-            print(f"{Person(self.name)} {your_personality}, you are not Cool...!!!")
-        else:
-            print("FUCK YOU...!!!")
-
-    def succsess(self, dedicated, prsistence, patient, passion):
-        dedicated = True
-        prsistence = True
-        patient = True
-        passion = True
-
+#######################################################################
+from time import sleep
+class You():
+	def __init__(you, name):
+		you.name = name
+	
+	def cool_person(you, your_personality):
+		your_atitude = ("hard worker", "humble", "nice", "good deed")
+		if your_personality in your_atitude:
+			print(f"{you.name} {your_personality}, you are cool...!!!")
+		elif your_personality not in your_atitude:
+			print("You are not cool...!!!")
+			
 def try_again():
-    pass
+    try:
+    	while True:
+    		never_giveup()
+    		if never_giveup() == True:
+    			you_alive()
+    		elif never_give() == False:
+    			try_harder()
+    		else:
+    			you_dead()
+    	else:
+    		you_dead()
+    except giveup() == True:
+    	you_dead()
 
 def try_harder():
-    pass
+    while  giveup() == False:
+    	continue
 
 def never_giveup():
     pass
@@ -32,7 +39,14 @@ def giveup():
     pass
 
 def you_dead():
-    pass
+    print("You are no longer needed...")
+    sleep(3)
+    print("You're not in here anymore...")
+    sleep(3)
+    print("Because you already die...!!!")
+    sleep(2)
+    print("Bye...")
+    exit()
 
 def you_alive():
     pass
@@ -40,17 +54,78 @@ def you_alive():
 def sadness():
     pass
 
-def happyness():
-    pass
+def happiness():
+    sleep(3)
+    print('''
+    "Being happy doesn't mean that everything is perfect.
+    It means that you've decided to look beyond the imperfections."
+    ''')
+    
+    sleep(3)
+    print('''
+    "Live simply. Give more. Expect less."
+    ''')
+    
+    sleep(3)
+    print('''
+    "If you want to live a happy life.
+    tie it to a goal, not to people or objects."
+    ''')
+    
+    sleep(3)
+    print('''
+    "Life is really simple, but we insist on making it complicated."
+    ''')
+    
+    sleep(3)
+    print('''
+    "You can never get enough of what you don't want."
+    ''')
+    
+    sleep(3)
+    print('''
+    "Happy people plan actions, they don't plan results."
+    ''')
+    
+    sleep(3)
+    print('''
+    "Whatever makes your soul happy, Do that...!!!"
+    ''')
 
-def cool_person(your_personality):
-    good = ("Hard Worker", "Humble", "kindess", "Nice", "Grateful")
-    if your_personality in good:
-        print("You are Cool...!!!")
-    elif your_personality not in good:
-        print("You are not Cool...!!!")
-    else:
-        print("FUCK YOU...!!!")
+def dedicated():
+	# sacrifice, loyality, integrity
+	print("Do it with sacrifice, loyality and integrity for your succsess")
+	sacrifice = ["your energy", "your mind", "your time"]
+	loyality = "kesetiaan dan kepatuhan aturan untuk tujuan kamu"
+	integrity = ("komitmen", "konsisten", "Bertanggung jawab")
+	efforts = 0
+	while True:
+		for you in sacrifice:
+			efforts += 1
+			print(you)
+			if efforts == 1:
+				print("usahamu mempunyai tujuan")
+				if "komitmen" in integrity:
+					print(integrity[0], ", maka hal itu tidak akan sia-sia")
+			elif efforts == 2:
+				print("diperlukan adanya sebuah keyakinan yang teguh.")
+				if "konsisten" in integrity:
+					print(integrity[1], ", walaupun harus dipaksa, meletihkan dan melelahkan")
+			elif efforts == 3:
+				print("menjunjung tinggi juga komitmen")
+				if "Bertanggung jawab" in integrity:
+					print("sekalipun jatuh di berbagai kesulitan")
+		break
+	print("hal tersebut menjadi bahan bakar semangat untuk bisa terus maju")
+	
+	#print("You are in doubt")
+	
+
+#dedicated()
+'''
+prsistence = True       # be presistence
+patient = True          # be patient
+passion = True   
 
 def succsess(dedicated, prsistence, patient, passion):
     you = ()
@@ -59,14 +134,31 @@ def succsess(dedicated, prsistence, patient, passion):
     patient = True          # be patient
     passion = True          # have passion
 
-    while True:
-        if you is dedicated:
-            print()
-        elif you is prsistence:
-            print()
-        elif you is patient:
-            print()
-        elif you is passion:
-            print()
-        else:
-            never_giveup()
+
+# ucup = You("Ucup")
+# print(ucup.cool_person("hard worker"))
+
+
+def life()
+
+'''
+happiness()
+you_dead()
+
+def life():	# by Kionk
+	if succsess != "key of":
+		happiness
+	elif happiness == "key":
+		for succsess in "life":
+			if succsessful == "You love what are you doing":
+			continue
+	key_to_life = (
+	"Put God First",
+	"Greathful",
+	"Feed the Poor",
+	"Love One Another",
+	"Appreciate the Fifference",
+	"Forgive Quickly",
+	"Live Simply",
+	"Stay Humble"
+	)
